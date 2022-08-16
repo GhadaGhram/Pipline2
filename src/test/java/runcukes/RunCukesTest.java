5f5ba817f17b8b67f50589e5fbb92893ad7a1d68
GhadaGhram <103033611+GhadaGhram@users.noreply.github.com>
2022-08-16 14:36:02 +0100

    Update RunCukesTest.java

--- src/test/java/runcukes/RunCukesTest.java
+++ src/test/java/runcukes/RunCukesTest.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/Features", strict=true , glue="stepdefs" , tags="@TEST_IX-1",
-        		plugin = {"pretty","html:target/HtmlReports"}
+        		plugin = {"pretty","html:target/HtmlReports","json:target/ghadag.json"}
 		)
 
 public class RunCukesTest {
